d63fcf0b9449c8c29f6f229c901c8844342e094d
Stefan Gherman <scgheman@gmail.com>
2020-01-20 04:16:03 +0200

    Add search algorithms in java script.

--- static/search.js
+++ static/search.js
@@ -14,6 +14,14 @@
  */
 function linear_search(number, list) {
 
+    let steps = 1
+    for (let i=0; i< list.length; i++){
+        if (list[i] === number){
+            return [i,steps];
+        }  else {
+            steps ++;
+        }
+    }
 
     return null;
 }
@@ -30,7 +38,28 @@ function linear_search(number, list) {
  */
 function binary_search(number, list) {
 
+    let left = 0;
+    let right = list.length-1;
+    let steps = 1;
+    while (left <= right){
+        mid = Math.floor((left + right)/2);
 
+        if (number === list[mid]){
+            return [mid, steps];
+        } else if (number === list[left]) {
+            return [left,steps];
+        } else if (number === list[right]) {
+            return [right, steps];
+        } else {
+            if (number > list[mid]) {
+                left = mid + 1;
+                steps ++;
+            } else if (number < list[mid]) {
+                right = mid - 1;
+                steps ++;
+            }
+        }
+    }
     return null;
 }
 
@@ -40,7 +69,7 @@ function print_result(search, index) {
     if (index === null) {
         result += 'did not find it.'
     } else {
-        result += `found it on index ${index}.`
+        result += `found it on index ${index[0]} in ${index[1]} steps.`
     }
 
     console.log(result);
@@ -49,6 +78,8 @@ function print_result(search, index) {
 
 function main() {
     let numbers = [3, 6, 8, 11, 18, 23, 24, 33, 36, 45, 46, 51, 56, 60, 69, 72, 83, 90, 93, 97];
+    console.log(numbers)
+
     let searched_number = Number(prompt('What is the number you are looking for? (0-100)'));
 
     print_result('Linear', linear_search(searched_number, numbers));
