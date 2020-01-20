4cedef549b6a8b727d8c5f66c05cfd3b923a5b14
Stefan Gherman <scgheman@gmail.com>
2020-01-20 04:18:29 +0200

    Create routes for java script functions.

--- main.py
+++ main.py
@@ -5,6 +5,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def return_home():
+    return render_template('home.html')
+
+@app.route('/fizzbuzz')
+def return_fizzbuzz():
     return render_template('fizzbuzz.html')
+
+@app.route('/search')
+def return_search():
+    return render_template('search.html')
+
 if __name__=='__main__':
     app.run(debug=True)
